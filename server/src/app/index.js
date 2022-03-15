0c14d06e902254caedc44e91c1bdbe9efd1b210a
ice-creamdog <1329467963@qq.com>
2022-03-15 23:44:39 +0800

    feat: 引入依赖，拆分app层和http层

---
+++ server/src/app/index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const Koa = require('koa')
+const KoaBody = require('koa-body')
+const KoaStatic = require('koa-static')
+const parameter = require('koa-parameter')
+
+const errHandler = require('./errHandler')
+const router = require('../router')
+
+
+
+
+
+const app = new Koa()
+
+app.use(
+    KoaBody({
+        multipart: true,
+        formidable: {
+            // uploadDir: path.join(__dirname, '../upload')
+            keepExtensions: true
+        },
+        parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
+    })
+)
+// app.use(KoaStatic(path.join(__dirname, '../upload')))
+app.use(parameter(app))
+app.use(router.routes()).use(router.allowedMethods())
+
+app.on('error', errHandler)
+
+module.exports = app
---
+++ server/src/router/login.router.js
@@ -0,0 +1,6 @@
+const Router = require('koa-router')
+
+
+const router = new Router({ prefix: '/users' })
+
+module.exports = router
